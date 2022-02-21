88b515054455efbe3d60c682825aef3aa22b03f7
Mateus Benites Dias <imattdias@gmail.com>
2022-02-20 21:09:34 -0300

    CTD - Back End I - Aula 01 - Async

--- src/Aula01/Person.java
+++ src/Aula01/Person.java
@@ -1,5 +1,57 @@
 package Aula01;
 
 public class Person {
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private int idade;
+
+    public Person(String nome, String sobrenome, String email, int idade) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void exibirNome(){
+        System.out.println(getSobrenome() + ", " + getNome());
+    }
+
+    public boolean maiorIdade(){
+        return getIdade() >= 18;
+    }
+
 
 }
---
+++ src/Aula01/PersonTest.java
@@ -0,0 +1,14 @@
+package Aula01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @org.junit.jupiter.api.Test
+    void exibirNome() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void maiorIdade() {
+    }
+}
